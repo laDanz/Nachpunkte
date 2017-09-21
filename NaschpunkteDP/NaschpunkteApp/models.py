7456a57e88d41ae50a1d3741339543d9ccfa286d
ladanz <cdanzmann@gmail.com>
2017-09-21 20:39:23 +0200

    first try models

--- NaschpunkteDP/NaschpunkteApp/models.py
+++ NaschpunkteDP/NaschpunkteApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+from google.appengine.ext import ndb
 # Create your models here.
+class Activity(ndb.Model):
+    name = ndb.StringProperty()
+    punkte = ndb.IntegerProperty()
+    id = ndb.StringProperty()
+    created = ndb.DateTimeProperty(auto_now_add=True)
