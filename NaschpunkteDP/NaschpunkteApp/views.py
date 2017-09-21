9b3c3a3b0d153b01b51673ce301378f9c297cfe5
ladanz <cdanzmann@gmail.com>
2017-09-21 20:54:57 +0200

    storage test

--- NaschpunkteDP/NaschpunkteApp/views.py
+++ NaschpunkteDP/NaschpunkteApp/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+from NaschpunkteApp.models import Activity
+import uuid
 
 def index(request):
+    a = Activity()
+    a.name="Test"
+    a.id=uuid.uuid1().hex
+    a.put()
     return HttpResponse(
         'Hello, World. This is Django running on Google App Engine')
